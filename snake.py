cf81e011985a47125e354103de2677882bdc1144
reistence <gvlamberti@gmail.com>
2023-03-01 11:47:34 +0100

    first commit

---
+++ snake.py
@@ -0,0 +1,10 @@
+"""Small Tkinter beginner project - Snake Game"""
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Tkinter - Snake")
+root.geometry('300x500+1200+40')
+
+
+root.mainloop()
